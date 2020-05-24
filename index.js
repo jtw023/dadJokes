378fd5e69a95c22e349c8b42864d9f75dba6e6b2
jtw023 <jordanwalters023@gmail.com>
2020-05-24 16:33:47 -0700

    cleaned code

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const jokeEl = document.getElementById('joke');
-const get_joke = document.getElementById('get_joke');
+// get joke functionality
+const jokeEl = document.getElementById("joke");
+const get_joke = document.getElementById("get_joke");
 
-get_joke.addEventListener('click', generateJoke);
+get_joke.addEventListener("click", generateJoke);
 
 generateJoke();
 
 async function generateJoke() {
-  const jokeRes = await fetch('https://icanhazdadjoke.com/', {
+  const jokeRes = await fetch("https://icanhazdadjoke.com/", {
     headers: {
-      'Accept': 'application/json'
-    }
+      Accept: "application/json",
+    },
   });
 
   const joke = await jokeRes.json();
 
-  console.log(joke);
-
   jokeEl.innerHTML = joke.joke;
-
 }
